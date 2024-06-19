39dda6cd2e788363a46bfe0d0df9f77048ccd842
Kalpanathmajhi <71187581+Kalpanathmajhi@users.noreply.github.com>
2024-06-19 23:15:16 +0530

    added docker

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express, { query } from "express"
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+
+
+async function init()  {
+const app = express();
+const PORT = Number(process.env.PORT) || 8000
+
+app.use(express.json())
+const gqlServer = new ApolloServer({
+    typeDefs: `
+    type Query {
+    hello:String
+    say(name:String):String
+    }
+    `, //Scema as string
+    resolvers: {
+        Query: {
+            hello: () => `Hey there, I am a Graphql server`,
+            say: (_, {name}: {name:String}) => `Hey ${name},How are you`
+        }
+    }, 
+})
+
+await gqlServer.start()
+//Start the gql server
+app.get('/', (req,res) => {
+    res.json({message:"Server is up and running"})
+});
+app.use("/graphql",expressMiddleware(gqlServer))
+app.listen(PORT, ()=> console.log(`Server started at PORT:${PORT}`));
+}
+init();
