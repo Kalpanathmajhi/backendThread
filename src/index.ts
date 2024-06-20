f396f0123125fc7676fbe729c64e2496bfd5806e
Kalpanathmajhi <71187581+Kalpanathmajhi@users.noreply.github.com>
2024-06-21 00:46:18 +0530

    added prisma schema

--- src/index.ts
+++ src/index.ts
@@ -3,32 +3,32 @@ import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 
 
-async function init()  {
-const app = express();
-const PORT = Number(process.env.PORT) || 8000
+async function init() {
+    const app = express();
+    const PORT = Number(process.env.PORT) || 8000
 
-app.use(express.json())
-const gqlServer = new ApolloServer({
-    typeDefs: `
+    app.use(express.json())
+    const gqlServer = new ApolloServer({
+        typeDefs: `
     type Query {
     hello:String
     say(name:String):String
     }
     `, //Scema as string
-    resolvers: {
-        Query: {
-            hello: () => `Hey there, I am a Graphql server`,
-            say: (_, {name}: {name:String}) => `Hey ${name},How are you`
-        }
-    }, 
-})
+        resolvers: {
+            Query: {
+                hello: () => `Hey there, I am a Graphql server`,
+                say: (_, { name }: { name: String }) => `Hey ${name},How are you`
+            }
+        },
+    })
 
-await gqlServer.start()
-//Start the gql server
-app.get('/', (req,res) => {
-    res.json({message:"Server is up and running"})
-});
-app.use("/graphql",expressMiddleware(gqlServer))
-app.listen(PORT, ()=> console.log(`Server started at PORT:${PORT}`));
+    await gqlServer.start()
+    //Start the gql server
+    app.get('/', (req, res) => {
+        res.json({ message: "Server is up and running" })
+    });
+    app.use("/graphql", expressMiddleware(gqlServer))
+    app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
 }
 init();
